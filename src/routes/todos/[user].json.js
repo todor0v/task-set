f77d809b27583516cdc3546510ca6116c81a28a1
Todor Todorov <84301735+todor0v@users.noreply.github.com>
2022-01-30 00:48:19 +0200

    small api check fix

--- src/routes/todos/[user].json.js
+++ src/routes/todos/[user].json.js
@@ -2,7 +2,7 @@
 import { api } from "./_api";
 
 export const get = (event) => {
-  if (event.request.headers.get('cookie').includes('authenticated=true')) return api(event);
+  if (event.request.headers.get('cookie')?.includes('authenticated=true')) return api(event);
   return {
     headers: { Location: '/' },
     status: 308
--- src/routes/todos/index.json.js
+++ src/routes/todos/index.json.js
@@ -3,7 +3,7 @@ import { api } from "./_api";
 
 export const post = async (event) => {
   const body = await event.request.json();
-  if (event.request.headers.get('cookie').includes('authenticated=true')) return api(event, {
+  if (event.request.headers.get('cookie')?.includes('authenticated=true')) return api(event, {
     created_at: new Date(),
     // description: request.body.get("description"),
     description: body.description,
